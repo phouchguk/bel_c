bef6b73bfe06b1cb0d00130d5f68118ba6b96d8e
Phouchg <phouchg@protonmail.com>
2020-04-23 09:50:08 +0100

    parse paren and quote tokens

--- bel.c
+++ bel.c
@@ -17,10 +17,36 @@ char is_whitespace(char c)
   return is_escape(c) || c == ' ';
 }
 
+char is_quote(char c)
+{
+  return c == '\'' || c == '`' || c == ',';
+}
+
+char is_paren(c)
+{
+  return c == '(' || c == ')' || c == '[' || c == ']';
+}
+
+char is_delimiter(char c)
+{
+  return is_whitespace(c) || is_paren(c) || is_quote(c);
+}
+
+char in_comma = 0;
+
 void parse_token()
 {
+  if (token[0] == ',' && token[1] == '\0') {
+    if (in_comma) {
+      in_comma = 0;
+    } else {
+      in_comma = 1;
+      return;
+    }
+  }
+
   printf(token);
-  putchar(NEWLINE);
+  putchar(' ');
 }
 
 char in_comment = 0;
@@ -40,15 +66,40 @@ void parse_char(char c)
     return;
   }
 
+  if (in_comma) {
+    /* checking next char for @ */
+    if (c == '@') {
+      in_comma = 0;
+
+      token[1] = '@';
+      token[2] = '\0';
+
+      parse_token();
+
+      /* don't continue parsing '@' */
+      return;
+    }
+
+    /* parse previous ',', continue parsing c */
+    parse_token();
+  }
+
   if (token_i == MAX_TOKEN) {
     printf("token too long -- PARSE_CHAR\n");
     exit(1);
   }
 
-  if (is_whitespace(c)) {
+  if (is_delimiter(c)) {
     if (token_i > 0) {
       token[token_i] = '\0';
       token_i = 0;
+
+      parse_token();
+    }
+
+    if (is_paren(c) || is_quote(c)) {
+      token[0] = c;
+      token[1] = '\0';
 
       parse_token();
     }
@@ -69,8 +120,6 @@ void main()
 
   if (token_i > 0) {
     token[token_i] = '\0';
-    token_i = 0;
-
     parse_token();
   }
 }
