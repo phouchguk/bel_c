e44633c2a8777043b6782ce78d3de32872b6344a
Phouchg <phouchg@protonmail.com>
2020-04-23 10:36:06 +0100

    start of char expect

--- bel.c
+++ bel.c
@@ -38,16 +38,16 @@ char in_comma = 0;
 void parse_token()
 {
   if (token[1] == '\0') {
-    if (token[0] == '\\') {
+    switch (token[0]) {
+    case '\\':
       if (in_char) {
         in_char = 0;
       } else {
         in_char = 1;
         return;
       }
-    }
 
-    if (token[0] == ',') {
+    case ',':
       if (in_comma) {
         in_comma = 0;
       } else {
@@ -62,6 +62,8 @@ void parse_token()
 }
 
 char in_comment = 0;
+char expect[3];
+int expect_i = 0;
 
 void parse_char(char c)
 {
@@ -75,6 +77,35 @@ void parse_char(char c)
 
   if (in_char) {
     /* need to check for \bel \cr \lf \sp \tab */
+    switch (c) {
+    case 'b':
+      expect[0] = 'e';
+      expect[1] = 'l';
+      expect[2] = '\0';
+      return;
+
+    case 'c':
+      expect[0] = 'r';
+      expect[1] = '\0';
+      return;
+
+    case 'l':
+      expect[0] = 'f';
+      expect[1] = '\0';
+      return;
+
+    case 's':
+      expect[0] = 'p';
+      expect[1] = '\0';
+      return;
+
+    case 't':
+      expect[0] = 'a';
+      expect[1] = 'b';
+      expect[2] = '\0';
+      return;
+    }
+
     in_char = 0;
 
     token[1] = c;
