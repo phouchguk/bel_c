9b5dc8669b245f4615b402e741fbfb45cce4711c
phouchg <phouchg@protonmail.com>
2020-04-25 14:47:12 +0100

    cell_shift constant for tcc

--- test.c
+++ test.c
@@ -2,37 +2,33 @@
 #include <stdlib.h>
 
 #define TAG_BIT_SIZE 3
+#define CELL_SHIFT 29
 
 typedef unsigned int cell;
 enum belt { SYM = 0, PAIR = 1, CHAR = 2, STREAM = 3, NUM = 4, HEART = 5 };
 
-const size_t bc = sizeof(size_t);
-const int cell_shift = (bc * 8) - TAG_BIT_SIZE;
-
 int is_self_evaluating(cell c)
 {
-  return c >> cell_shift > 1;
+  return c >> CELL_SHIFT > 1;
 }
 
 int main()
 {
-  /* filled with 1s */
   const cell x = ~0U;
   const cell data_mask = x >> TAG_BIT_SIZE;
-
   const cell sym_mask    = 0;
-  const cell pair_mask   = PAIR << cell_shift;
-  const cell char_mask   = CHAR << cell_shift;
-  const cell stream_mask = STREAM << cell_shift;
-  const cell num_mask    = NUM << cell_shift;
-  const cell heart_mask  = HEART << cell_shift;
+
+  const cell pair_mask   = PAIR << CELL_SHIFT;
+  const cell char_mask   = CHAR << CELL_SHIFT;
+  const cell stream_mask = STREAM << CELL_SHIFT;
+  const cell num_mask    = NUM << CELL_SHIFT;
+  const cell heart_mask  = HEART << CELL_SHIFT;
 
   const cell nil = 0 | sym_mask;
   const cell p = 0 | pair_mask;
   const cell c = 'g' | char_mask;
   const cell n = 42 | num_mask;
 
-  printf("%i\n", bc);
   printf("x:           %u\n", x);
   printf("data_mask:   %u\n", data_mask);
   printf("sym_mask:    %u\n", sym_mask);
@@ -52,10 +48,10 @@ int main()
   printf("val c: %i\n", c & data_mask);
   printf("val n: %i\n", n & data_mask);
 
-  printf("t nil: %i\n", nil >> cell_shift);
-  printf("t p: %i\n", p >> cell_shift);
-  printf("t c: %i\n", c >> cell_shift);
-  printf("t n: %i\n", n >> cell_shift);
+  printf("t nil: %i\n", nil >> CELL_SHIFT);
+  printf("t p: %i\n", p >> CELL_SHIFT);
+  printf("t c: %i\n", c >> CELL_SHIFT);
+  printf("t n: %i\n", n >> CELL_SHIFT);
 
   printf("se nil: %i\n", is_self_evaluating(nil));
   printf("se p: %i\n", is_self_evaluating(p));
